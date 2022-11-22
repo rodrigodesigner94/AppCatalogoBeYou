e9aa96b1db23d8e986b17519c1dea3e3b5880aea
Rodrigo Borges <digodesign94@gmail.com>
2022-11-22 12:36:04 -0300

    feature: delete button implementation

--- src/pages/home/componentsH/productCard.jsx
+++ src/pages/home/componentsH/productCard.jsx
@@ -18,23 +18,21 @@ function ProductCard({name, price, image, description, color, size, bImage}) {
         <Card.Title style={{textShadow: "0.8px 0.8px 1px white"}}>{name}</Card.Title>
         <Card.Subtitle className="text-black" style={{textShadow: "1.5px 1.5px 1px white"}}>
           {price}</Card.Subtitle>        
-        <div 
-    // style={{ minHeight: '10rem' }}
-    >
-        <Collapse in={open} dimension="width">
+        
+        <Collapse in={open} dimension="width" style={{marginLeft:"-1.8rem"}}>
           <div id="colapseText" >
-            <Card body style={{ width: '15rem',zIndex: "1" }}>
+            <Card body style={{ width: '13.5rem',zIndex: "1" }}>
               <Container className='d-flex' style={{justifyContent:"end", padding: "0"}}>
                 <img src="https://cdn-icons-png.flaticon.com/512/88/88039.png?w=740&t=st=1669119726~exp=1669120326~hmac=38176d558c65f8ebdf830f2efb86e0461701cf27c10a25d06a4147017aea54c8" height={"12rem"} /> 
                 </Container>
               <Card.Text style={{textShadow: "1.5px 1.5px 1px white"}}><strong>Cor:</strong> {color}</Card.Text>
               <Card.Text style={{textShadow: "1.5px 1.5px 1px white"}}><strong>Tamanho:</strong> {size}</Card.Text>
               <Card.Text style={{textShadow: "1.5px 1.5px 1px white"}}><strong>descrição:</strong> {description}</Card.Text>
-              <Card.Img width={"2rem"} src={bImage} alt="costas" />
+              <Card.Img style={{width: "90%"}} src={bImage} alt="costas" />
             </Card>
           </div>
         </Collapse>
-      </div>
+      
         {/* <Button style={{opacity:"60%"}} variant="primary">Quero esse</Button>        */}
       </Card.ImgOverlay>      
       
